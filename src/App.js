e2d15b29e60d0874aca71f3b0246285a677a6c1c
Saroj Sasamal <sarojsasmal990@gmail.com>
2022-08-13 18:10:28 +0530

    add tailwind css and layouts (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Meal from "./components/Meal";
+import { useState } from "react";
+import { Sidebar } from "./components/pages/Sidebar";
 
 function App() {
+  const [open, setOpen] = useState(true);
   return (
-    <div>
-      <div>
+    <div className="flex">
+      <div
+        className={`bg-indigo-500 h-screen p-5 pt-8 ${
+          open ? "w-1/6" : "w-20"
+        } duration-300 relative`}
+      >
+        <Sidebar open={open} onClick={setOpen} />
+      </div>
+      <div className={`p-7 ${open ? "w-5/6" : "w-full"}`}>
+        <h1 className="text-2xl font-semibold">Home Page</h1>
         <Meal />
       </div>
     </div>
---
+++ src/constants/menu.js
@@ -0,0 +1,22 @@
+export const menu = [
+  {
+    name: "Dashboard",
+    key: "dashboard",
+  },
+  {
+    name: "Profile",
+    key: "Profile",
+  },
+  {
+    name: "Preferences",
+    key: "preferences",
+  },
+  {
+    name: "Report",
+    key: "report",
+  },
+  {
+    name: "User",
+    key: "user",
+  },
+];
