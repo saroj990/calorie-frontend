45729300098c6a70095d0638dc605f895266b131
Saroj Sasamal <sarojsasmal990@gmail.com>
2022-08-29 19:53:03 +0530

    css changes for sidebar and layout

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex w-full">
       <Sidebar />
-      <main className="p-4 w-11/12 bg-slate-600 text-white min-h-screen g-full">
+      <main className="p-4 w-11/12 bg-white text-black min-h-screen g-full">
         {children}
       </main>
     </div>
